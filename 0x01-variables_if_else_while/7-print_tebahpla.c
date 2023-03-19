d11c8b9dc52948ec5c3dbe99155fd6482732fe52
DAGAN Babatounde V Idelphonse <daganbabatounde@gmail.com>
2023-03-19 09:51:46 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Prints the lowercase alphabet in reverse
+ *
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
